9f88412f82ff6e02df6a92bfa16ed071c895b0ff
Party4Bread <party4bread@gmail.com>
2018-12-03 14:14:07 +0900

    Add files via upload

--- Command.ts
+++ Command.ts
@@ -5,19 +5,3 @@ interface Command{
     flag:Number,
     toHyeong:Function
 }
-
-class Mul implements Command{
-    arg1: number;
-    arg2: number;
-    flag: number;
-    isFlaged:Boolean;
-    toHyeong: Function;
-    constructor(a:number,b:number,c?:number){
-        this.arg1=a;
-        this.arg2=b;
-        if(c!==undefined&&c!==null){
-            this.isFlaged=true;
-            this.flag=c
-        }
-    }
-}

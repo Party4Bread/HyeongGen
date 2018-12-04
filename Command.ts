788ba7b60a02a2234f3d19a3ed5c10123d770177
든솔 이 <solsam10@outlook.kr>
2018-12-04 10:36:44 +0900

    Chk Err

--- Command.ts
+++ Command.ts
@@ -4,6 +4,7 @@ abstract class Command{
     isFlaged:boolean;
     flag:number;
     constructor(a:number,b:number,c?:number){
+        if (a===0)throw "첫번째 인자 값은 0일수 없습니다.";
         this.arg1=a;
         this.arg2=b;
         if(c!==undefined&&c!==null){
